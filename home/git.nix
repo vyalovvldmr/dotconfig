1b302f094317bf8aa6044c283f51ff9103d0f194
Vladimir Vialov <vyalov.v@gmail.com>
2025-02-11 13:43:35 +0100

    add git last alias

--- home/git.nix
+++ home/git.nix
@@ -41,6 +41,7 @@
       lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
       c = "commit -am";
       amend = "commit --amend -m";
+      last = "git log --oneline -n 1";
     };
   };
 }
