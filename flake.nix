ded019764e11e4398471fdc85220ee54dd1925be
Vladimir Vialov <vyalov.v@gmail.com>
2025-03-08 12:01:20 +0100

    Fix nix deps

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin = {
-      url = "github:lnl7/nix-darwin";
+      url = "github:lnl7/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
